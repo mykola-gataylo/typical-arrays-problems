cc1c1c728337fbbad2b21ffb44677a2790f9fe73
Mykola Gataylo <mykola.gataylo@gmail.com>
2021-03-09 15:57:10 +0100

    fix: fix typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!Array.isArray(array)) {
+        return 0;
+    } else if (array.length === 0) {
+        return 0;
+    } else {
+        return Math.min.apply(Math, array);
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!Array.isArray(array)) {
+        return 0;
+    } else if (array.length === 0) {
+        return 0;
+    } else {
+        return Math.max.apply(Math, array);
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!Array.isArray(array)) {
+        return 0;
+    } else if (array.length === 0) {
+        return 0;
+    } else {
+        let sum = 0;
+
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        let avg = sum / array.length;
+        return avg;
+    }
 }
